d55dcf2378ca8d609bb5bf06e561d02b80a77ff2
RyanmjDev <Ryanmj.dev@gmail.com>
2023-11-27 16:39:37 -0600

    implements ChatInput adding to message state

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux'
+import { addMessage } from '../features/chat/chatSlice';
 
 const ChatInput = () => {
+const [input, setInput] = useState ('')
+const dispatch = useDispatch();
+
+const handleSubmit = () => {
+  console.log("Submitted!")
+  if(input.trim())
+  {
+    dispatch(addMessage({message: input, isUser:true}));
+    setInput('');
+  }
+};
+
+const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+  if (event.key === 'Enter') {
+    handleSubmit();
+  }
+};
+
   return (
     <div className='fixed inset-x-0 bottom-3 mx-auto max-w-lg md:w-full h-8 flex 
     justify-between '>
-        
+      
         <input type="text"
                className='bg-slate-700 text-white w-full p-2 box-border rounded-2xl'
+               value={input}
+               onChange={(e) => setInput(e.target.value)}
+               onKeyPress={handleKeyPress}
+               placeholder="Type a message..."
         />
     </div>
   );
