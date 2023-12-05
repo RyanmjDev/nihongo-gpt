ad01651bbc9f21a2004ace1e24adf0ee4ecd303e
RyanmjDev <Ryanmj.dev@gmail.com>
2023-12-05 15:58:38 -0600

    Fixes bug with User Chatbubbles

--- src/components/ChatBubble/ChatBubble.tsx
+++ src/components/ChatBubble/ChatBubble.tsx
@@ -4,17 +4,18 @@ import { ChatMessage } from '../../types/types';
 
 
 const ChatBubble: React.FC<ChatMessage> = ({ message, isUser }) => {
-  const userStyles = "bg-blue-500 ml-auto text-right rounded-bl-xl";
-  const aiStyles = "bg-gray-700 mr-auto text-left rounded-br-xl";
+  const userStyles = "bg-blue-500 text-right rounded-bl-xl";
+  const aiStyles = "bg-gray-700 text-left rounded-br-xl";
 
   const bubbleStyles = `text-white text-xl px-2 py-1 max-w-xs lg:max-w-md rounded-tl-xl rounded-tr-xl mb-2 ${
     isUser ? userStyles : aiStyles
   }`;
 
   return (
-    <div className={bubbleStyles} >
-      
-      {message}
+    <div className={`flex ${isUser ? 'justify-end' : ''}`}>
+      <div className={bubbleStyles} >
+        {message}
+      </div>
     </div>
   );
 };
