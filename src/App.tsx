130f4ba3222921c3b0eaaa4f404374262c3315a3
RyanmjDev <Ryanmj.dev@gmail.com>
2023-12-06 13:33:21 -0600

    Update background color and add LoginPage component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { useState } from 'react'
 import './App.css'
 import ChatInput from './components/ChatInput/ChatInput'
 import ChatList from './components/ChatList/ChatList'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
 
   return (
     <>
-      <div className='flex flex-col items-center'>
+      {/* <div className='flex flex-col items-center'>
         <ChatList/>
 
       </div>
-      <ChatInput/>
+      <ChatInput/> */}
+
+      <LoginPage/>
 
     </>
   )
--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -1,26 +1,70 @@
 import React, { useState } from 'react';
-import { loginUser } from '../../services/api';
-import { useNavigate } from 'react-router-dom';
-
-type Credentials = {
-    username: string;
-    password: string;
-};
-
+import axios from 'axios';
 
 const Login = () => {
-    const [credentials, setCredentials] = useState<Credentials>({ username: '', password: '' });
-    const navigate = useNavigate();  
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleLogin = async () => {
-        try {
-            const user = await loginUser(credentials);
-            navigate('/chat'); 
-        } catch (error) {
-            console.log(error)
-        }
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
     };
-    
 
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+                
+    };
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <form className="bg-gray-900 shadow-md rounded-lg px-10 py-12 mb-4">
+                <div className="mb-6">
+                    <label className="block text-gray-300 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight bg-gray-800 focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block text-gray-300 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight bg-gray-800 focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                        <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-2"
+                                type="submit"
+                        >
+                                Login In
+                        </button>
+                        <button
+                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                                type="button"
+                                >
+                                Register
+                        </button>
+                </div>
+            </form>
+        </div>
+    );
 };
+
+export default Login;
